c79590a09e68bc4bfd4ee285a9688705d2bc2149
Radek Szczesny <radek.szczesny@pearson.com>
2024-01-16 14:56:00 +0100

    network

--- network/from_device_list.py
+++ network/from_device_list.py
@@ -1,4 +1,3 @@
-import re # import regular expression module
 import time # import time module
 import paramiko # import paramiko library
 from datetime import datetime # import datetime module from datetime library
@@ -6,41 +5,25 @@ import confirm_password
 
 confirm_password.get_credentials() # Run get_credentials() function
 
+def disable_paging(show_vers): #Disable paging on a Cisco router
+    show_vers.send("terminal length 0\n")
+    time.sleep(1)# Clear the buffer on the screen
+    output = show_vers.recv(1000)
+    return output
+
 def login():
+    global show_vers
     global ssh_client
     ssh_client = paramiko.SSHClient()# Initiate paramiko SSH client session as ssh_client
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # Accept missing host key policy
     ssh_client.connect(hostname=ip,username=confirm_password.user,password=confirm_password.password, allow_agent=False,look_for_keys=False, timeout=60) # SSH connection with credentials
-    #stdin, stdout, stderr  = ssh_client.exec_command('terminal length 0\n')
-    #mystring = stdout.read()
-    #print(mystring.decode('utf-8'))
-    #output1 = show_vers.recv(1000)
-    #stdin, stdout, stderr  = ssh_client.exec_command('show version\n')
-    #time.sleep(3)
-    #show_version = stdout.read()
-    #print(show_version.decode('utf-8'))
-    #term0 = terminal.recv(1000)
-    #print(term0.decode('utf-8'))
     show_vers = ssh_client.invoke_shell()
     time.sleep(1)
-    #stdin, stdout, stderr  = ssh_client.exec_command('terminal length 0\n')
-    #mystring = stdout.read()
-    #print(mystring.decode('utf-8'))
-    show_vers.send("term len 0\n")
-    show_vers.send("clear\n")
-    #show_clock = show_vers.recv(1000)
-    #print(show_clock.decode('utf-8'))
+    disable_paging(show_vers)
     time.sleep(1)
-    output = show_vers.send("show version\n")
-    time.sleep(3)
-    #device = show_vers.recv(60000)
-    print(output)
-        
-def show_version():
-    show_vers = ssh_client.invoke_shell()
     show_vers.send("show version\n")
     time.sleep(3)
-    device = show_vers.recv(6000)
+    device = show_vers.recv(60000)
     print(device.decode('utf-8'))
 
 start_timer = time.mktime(time.localtime()) # Start time
@@ -50,6 +33,7 @@ file=open("list.txt", "r", encoding="utf8", errors='ignore')
 for line in file.readlines():
     t_ref = datetime.now().strftime("%Y-%m-%d %H-%M-%S") # Time reference to be used for file name
     print(t_ref)
+    print()
     ip=line.strip() #remove unnecessary characters
     login()
     print("Logged to " + ip)
